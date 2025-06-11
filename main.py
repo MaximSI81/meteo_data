a3511c7030dc5142752c0a24e77f9b29fdd359a2
Maksim Safonov <safonovmio@gmail.com>
2025-06-11 18:52:03 +0000

    добавил docker-compose

--- main.py
+++ main.py
@@ -1,3 +1,41 @@
-class StreetTreeCensus:
-    def __init__(self):
-        pass
+
+import duckdb
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+
+conn_str = (
+    f"host={os.getenv('DB_HOST')} "
+    f"port={os.getenv('DB_PORT')} "
+    f"dbname={os.getenv('DB_NAME')} "
+    f"user={os.getenv('DB_USER')} "
+    f"password={os.getenv('DB_PASSWORD')}"
+)
+
+PATH = './data/'
+
+
+attach_result = duckdb.sql(f"ATTACH '{conn_str}' AS pg_db (TYPE postgres)")
+
+
+
+
+
+
+
+
+
+
+
+
+def main():
+    pass
+
+
+
+
+
+if __name__ == '__main__':
+    main()

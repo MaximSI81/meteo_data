454465e71b768dd07d2fd33909aba91c4d02b54b
Maksim Safonov <safonovmio@gmail.com>
2025-06-21 20:40:31 +0000

    исправил добавление по колонкам clouds, gust, rain, snow и разложил по папкам городов в s3

--- airflow/dags/get_meteo_data.py
+++ airflow/dags/get_meteo_data.py
@@ -62,26 +62,26 @@ def get_data_openweathermap(**context):
         responce = requests.get(url=URL)  # запрос к openWeatherMap
         data = responce.json()  # ответ в json
         df = pd.DataFrame([{**data['main'], **data['wind']}])
-        df['clouds'] = data['clouds']
-        df['gust'] = pd.NA if 'gust' not in data['main'] else df['gust']
-        df['rain'] = pd.NA if 'rain' not in data else data['rain']
-        df['snow'] = pd.NA if 'snow' not in data else data['snow']
+        df['clouds'] = data['clouds']['all']
+        df['gust'] = pd.NA if 'gust' not in data['wind'] else df['gust']
+        df['rain'] = pd.NA if 'rain' not in data else data['rain']['1h']
+        df['snow'] = pd.NA if 'snow' not in data else data['snow']['1h']
         df.insert(0, 'date', context["date_time"])  # добавляем дату и время запроса
         try:
-            responce_minio = client_minio.get_object(bucket_name = "prod", object_name = f"{city}_{context['date']}_temp.parquet") # получаем файл из minio если существует
+            responce_minio = client_minio.get_object(bucket_name = "prod", object_name = f"{city}/{city}_{context['date']}_temp.parquet") # получаем файл из minio если существует
             parquet_buffer = BytesIO(responce_minio.read()) # читаем и создаем буфер - файлоподобный объект
             responce_minio.close()  # закрываем соеденение
             responce_minio.release_conn() # Освобождает соединение в пуле
             df = pd.concat([pd.read_parquet(parquet_buffer), df])  # объеденяем прочитанный df с новым
         except S3Error as error:
-            print(f"Файл {city}_{context['date']}_temp.parquet не найден - {error}")
+            print(f"Файл {city}/{city}_{context['date']}_temp.parquet не найден - {error}")
         parquet_buffer_new = BytesIO() #  создаем новый буфер
         df.to_parquet(parquet_buffer_new, engine='pyarrow', index=False) # записываем parquet файл в буфер
         parquet_buffer_new.seek(0)  # Сбрасываем позицию чтения
             # загружаем в minio
         client_minio.put_object(
             bucket_name = BUCKET_NAME,
-            object_name = f"{city}_{context['date']}_temp.parquet",
+            object_name = f"{city}/{city}_{context['date']}_temp.parquet",
             data=parquet_buffer_new,
             length=parquet_buffer_new.getbuffer().nbytes,
             content_type='application/parquet',

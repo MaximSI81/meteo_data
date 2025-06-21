f681559c27751b747d0eda696ac707a5b7239188
Maksim Safonov <safonovmio@gmail.com>
2025-06-21 19:08:07 +0000

    изменил даг для загрузки по нескольким городам и небольшая оптимизация кода создания df

--- airflow/dags/get_meteo_data.py
+++ airflow/dags/get_meteo_data.py
@@ -48,7 +48,7 @@ client_minio = Minio(
 
 
 args = {'owner': OWNER, 
-        'start_date': pendulum.datetime(2025, 6, 18, tz="Europe/Moscow"),
+        'start_date': pendulum.datetime(2025, 6, 20, tz="Europe/Moscow"),
         "catchup": False,
         "retries": 1,
         "retry_delay": pendulum.duration(hours=1),}
@@ -57,33 +57,35 @@ args = {'owner': OWNER,
 
 def get_data_openweathermap(**context):
     ''' получам и записываем данные в minio '''
-    URL = f'https://api.openweathermap.org/data/2.5/weather?q={context["city_name"]}&appid={API_KEY}&units=metric'
-    responce = requests.get(url=URL)  # запрос к openWeatherMap
-    data = responce.json()  # ответ в json
-    df_temp = pd.DataFrame([data['main']])  # создаем dataframe погода
-    df_wind = pd.DataFrame([data['wind']])  # создаем dataframe ветер
-    df_temp.insert(0, 'date', context["date_time"])  # добавляем дату и время запроса
-    df_temp[['deg', 'speed', 'gust']] = df_wind[['deg', 'speed', 'gust']]  # объеденяем в общий df  
-    df = df_temp[['date', 'temp', 'feels_like', 'temp_min', 'temp_max', 'pressure', 'deg', 'speed', 'gust',]]
-    try:
-        responce_minio = client_minio.get_object(bucket_name = "prod", object_name = f"{context['city_name']}_{context['date']}_temp.parquet") # получаем файл из minio если существует
-        parquet_buffer = BytesIO(responce_minio.read()) # читаем и создаем буфер - файлоподобный объект
-        responce_minio.close()  # закрываем соеденение
-        responce_minio.release_conn() # Освобождает соединение в пуле
-        df = pd.concat([pd.read_parquet(parquet_buffer), df])  # объеденяем прочитанный df с новым
-    except S3Error as error:
-        print(f"Файл {context['city_name']}_{context['date']}_temp.parquet не найден - {error}")
-    parquet_buffer_new = BytesIO() #  создаем новый буфер
-    df.to_parquet(parquet_buffer_new, engine='pyarrow', index=False) # записываем parquet файл в буфер
-    parquet_buffer_new.seek(0)  # Сбрасываем позицию чтения
-        # загружаем в minio
-    client_minio.put_object(
-        bucket_name = BUCKET_NAME,
-        object_name = f"{context['city_name']}_{context['date']}_temp.parquet",
-        data=parquet_buffer_new,
-        length=parquet_buffer_new.getbuffer().nbytes,
-        content_type='application/parquet',
-    )  
+    for city in context['city_name']:
+        URL = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric'
+        responce = requests.get(url=URL)  # запрос к openWeatherMap
+        data = responce.json()  # ответ в json
+        df = pd.DataFrame([{**data['main'], **data['wind']}])
+        df['clouds'] = data['clouds']
+        df['gust'] = pd.NA if 'gust' not in data['main'] else df['gust']
+        df['rain'] = pd.NA if 'rain' not in data else data['rain']
+        df['snow'] = pd.NA if 'snow' not in data else data['snow']
+        df.insert(0, 'date', context["date_time"])  # добавляем дату и время запроса
+        try:
+            responce_minio = client_minio.get_object(bucket_name = "prod", object_name = f"{city}_{context['date']}_temp.parquet") # получаем файл из minio если существует
+            parquet_buffer = BytesIO(responce_minio.read()) # читаем и создаем буфер - файлоподобный объект
+            responce_minio.close()  # закрываем соеденение
+            responce_minio.release_conn() # Освобождает соединение в пуле
+            df = pd.concat([pd.read_parquet(parquet_buffer), df])  # объеденяем прочитанный df с новым
+        except S3Error as error:
+            print(f"Файл {city}_{context['date']}_temp.parquet не найден - {error}")
+        parquet_buffer_new = BytesIO() #  создаем новый буфер
+        df.to_parquet(parquet_buffer_new, engine='pyarrow', index=False) # записываем parquet файл в буфер
+        parquet_buffer_new.seek(0)  # Сбрасываем позицию чтения
+            # загружаем в minio
+        client_minio.put_object(
+            bucket_name = BUCKET_NAME,
+            object_name = f"{city}_{context['date']}_temp.parquet",
+            data=parquet_buffer_new,
+            length=parquet_buffer_new.getbuffer().nbytes,
+            content_type='application/parquet',
+        )  
 
 with DAG(
     dag_id=DAG_ID,
@@ -99,7 +101,7 @@ with DAG(
         task_id="get_weather_data",
         python_callable=get_data_openweathermap,
         op_kwargs={
-            "city_name": "Moscow",
+            "city_name": ["Moscow", "Minsk", "Saint Petersburg", "Kazan"],
             "date_time": '{{ ts }}',
             "date": '{{ ds }}'
         }
